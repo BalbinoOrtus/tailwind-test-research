e3a3f5aa6558a930a848566a70f58970db4298aa
Balbino Aylagas <baylagas@ortussolutions.com>
2022-01-27 16:33:35 -0600

    added grid01 html with link to video

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
       padding: "2rem",
     },
+    screens: {
+      xs: "540px",
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }
